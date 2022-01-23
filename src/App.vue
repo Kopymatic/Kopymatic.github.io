d6a9e649ccdf94bebac6dc4fdfe72d55e401f6c8
Kopy <creepcipher@gmail.com>
2022-01-23 00:11:01 -0700

    css make my head hurt

--- src/App.vue
+++ src/App.vue
@@ -34,43 +34,8 @@ a.no-underline {
     text-decoration: none;
 }
 
-button {
-    font-size: medium;
-    font-family: "Balsamiq Sans", cursive;
-    border-color: rgb(212, 3, 29);
-    border-width: 4px;
-    border-style: groove;
-    border-radius: 16px;
-    width: 90%;
-    height: 90%;
-    transition-duration: 0.4s;
-    background-color: rgb(48, 48, 48);
-    color: white;
-}
-
-#logo {
-    color: rgb(212, 3, 29);
-    font-family: "Rock Salt", cursive;
-    font-size: 300%;
-}
-
-button:hover {
-    background-color: black;
-    width: 95%;
-    height: 95%;
-}
-
 #subtitle {
     font-size: 200%;
-}
-
-#carin-buttons {
-    height: 100%;
-    padding: 5px 100px;
-    display: grid;
-    grid-gap: 20px;
-    grid-auto-rows: 1fr;
-    grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
 }
 
 .center {
