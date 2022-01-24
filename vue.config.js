99044932444a04b67c82839e955a07cd303546f2
Kopy <creepcipher@gmail.com>
2022-01-23 22:36:39 -0700

    e

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     // publicPath: "/home/",
+    // pwa: {
+    //     themeColor: "#d4031d",
+    //     msTileColor: "#d4031d",
+    //     appleMobileWebAppCache: "yes",
+    //     manifestOptions: {
+    //         background_color: "#d4031d",
+    //     },
+    // },
 };
