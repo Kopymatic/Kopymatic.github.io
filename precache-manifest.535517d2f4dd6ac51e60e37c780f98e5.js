5f6c7bed066268e0c1785ad4664fed9041dcda4e
Kopy <kopymatic@gmail.com>
2022-01-24 18:48:04 -0700

    deploy

---
+++ precache-manifest.535517d2f4dd6ac51e60e37c780f98e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd8961bb066d3ade2d45b54fbcc90d0",
+    "url": "/404.html"
+  },
+  {
+    "revision": "bd90693b6f781f1a5d89",
+    "url": "/css/app.27684d3c.css"
+  },
+  {
+    "revision": "4cbbe10340159bacbe2b4fba34a9352a",
+    "url": "/img/Kopy.png"
+  },
+  {
+    "revision": "5143e54a9e864d6b7140f279eb613e5d",
+    "url": "/img/hampter.5143e54a.jpg"
+  },
+  {
+    "revision": "f603d58a365003c543cb226a5f834b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd90693b6f781f1a5d89",
+    "url": "/js/app.c1f5c34d.js"
+  },
+  {
+    "revision": "337adc3115d28eace53c",
+    "url": "/js/chunk-vendors.0aec71ad.js"
+  },
+  {
+    "revision": "06a8a6e3e7658d8b5f2b8fe5f9ed17cb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
