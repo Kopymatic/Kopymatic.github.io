9680e089b93ab6f826b2cb488c2073b63c2167a1
Kopy <creepcipher@gmail.com>
2022-01-24 18:46:18 -0700

    i might be smart idk

--- src/components/SocialsFooter.vue
+++ src/components/SocialsFooter.vue
@@ -1,17 +1,28 @@
 <template>
-    <div class="center" id="socials">
-        <a href="https://twitter.com/Kopymatic" class="fab fa-twitter fa-lg" />
+    <div class="center" id="container">
+        <a href="https://twitter.com/Kopymatic" class="fab fa-twitter fa-lg item" />
         <a
             href="https://www.youtube.com/channel/UCF5_Evvi_JP-j12t-8YbOEA"
-            class="fab fa-youtube fa-lg"
+            class="fab fa-youtube fa-lg item"
         />
-        <a href="https://discord.gg/QXFkQ8RHMw" class="fab fa-discord fa-lg" />
-        <a href="https://github.com/Kopymatic" class="fab fa-github fa-lg" />
+        <a href="https://discord.gg/QXFkQ8RHMw" class="fab fa-discord fa-lg item" />
+        <a href="https://github.com/Kopymatic" class="fab fa-github fa-lg item" />
     </div>
 </template>
 
 <style scoped>
 #socials {
     font-size: 20px;
+}
+
+#container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+}
+
+.item {
+    flex: 0;
 }
 </style>
